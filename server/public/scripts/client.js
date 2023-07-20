0ce9e651ec036a14f35e71222d40b50775959d96
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-20 15:03:27 -0500

    client.js/server.js  updated

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,58 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+  // console.log("jquery is loaded!")
+
+  //HANDLER
+  $('#submitBtn').on('click', submitGuess)
+  $('#restartbtn').on('click', restartGame)
+
+}
+
+// function for submit 
+function submitGuess () {
+  const newGuess =  {
+    player1guess:6,
+    player2guess:9
+  }
+ 
+  $.ajax({
+    method:"POST",
+    url:"/addguess",
+    data:newGuess
+  }).then((response)=>{
+    getGuess()
+    render()
+  }).catch((error) =>{
+    console.log('Error with post',error);
+    alert('Error with post')
+  })
+
+  let render =() => {
+    $('#playerTable').empty();
+    for(let guess of guesses){
+      console.log(guess);
+      $('#playerTable').append(` 
+        <tr>
+        <td>${guess.player1guess}</td>
+        <td>${guess.player2guess}</td>
+        </tr>
+      `)
+    }
+  }
+
+
+
+
+
+
+
+
+
+}
+
+
+//function for restart
+function restartGame (){
+  console.log('restart is working too!!')
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,28 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+// Guess Array
+let playersGuess = [
+  {
+    player1guess:4,
+    player2guess:9
+  }
+]
 
+let randomNum = Math.floor(Math.random () * (25 - 1) +1)
+
+
+// GET & POST Routes go here
+app.get('/getguess', (req, res) => {
+  console.log('inside GET of guess')
+  res.send(200)
+})
+
+app.post('/addguess',(req,res) => {
+  let guessToAdd = req.body;
+  playersGuess.push(guessToAdd);
+  res.sendStatus(201);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)

574f123f2717f6fab975d27f3ecfb5a3048aaf6e
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-07-21 12:06:01 -0500

    almost there

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,71 +6,115 @@ function handleReady() {
   // console.log("jquery is loaded!")
 
   //HANDLER
-  $('#submitBtn').on('click', submitGuess)
-  $('#restartbtn').on('click', restartGame)
+  $('#submitBtn').on('click', submitGuess);
+  $('#restartbtn').on('click', restartGame);
 
   getGuess()
+
 }
 
 // function for submit 
-function submitGuess () {
-  const newGuess =  {
-    player1guess:6,
-    player2guess:9
+function submitGuess() {
+  let newGuess = {
+    player1guess: $('#player1').val(),
+    player2guess: $('#player2').val()
   }
- 
+
+
   $.ajax({
-    method:"POST",
-    url:"/addguess",
-    data:newGuess
-  }).then((response)=>{
+    method: "POST",
+    url: "/addguess",
+    data: newGuess
+  }).then((response) => {
     getGuess()
     render()
-  }).catch((error) =>{
-    console.log('Error with post',error);
+  }).catch((error) => {
+    console.log('Error with post', error);
     alert('Error with post')
   })
+}
 
-
-  let getGuess = () => {
-    $.ajax({
-      method: "GET",
-      url: '/getguess',
-    }).then((response) => {
-      guesses = response
-      render ()
-    }).catch ((error) => {
-      console.log('Error with GET', error)
-      alert('Error with GET')
-    })
-  }
-
-
-  let render =() => {
-    $('#playerTable').empty();
-    for(let guess of guesses){
-      console.log(guess);
-      $('#playerTable').append(` 
-        <tr>
-        <td>${guess.player1guess}</td>
-        <td>${guess.player2guess}</td>
-        </tr>
-      `)
-    }
-  }
-
-
-
-
-
-
-
-
-
+let getGuess = () => {
+  $.ajax({
+    method: "GET",
+    url: '/getguess',
+  }).then((response) => {
+    guesses = response
+    render()
+  }).catch((error) => {
+    console.log('Error with GET', error)
+    alert('Error with GET')
+  })
 }
 
 
+let render = () => {
+  $('#playerTable').empty();
+  let randomNum = 7;
+  for (let guess of guesses) {
+    console.log(guess);
+    $('#playerTable').append(` 
+        <tr>
+        <td class = 'player1td'>${guess.player1guess}</td>
+        <td class = 'player2td'>${guess.player2guess}</td>
+        </tr>
+       `)
+    if (guess.player1guess > randomNum) {
+      $('#playerTable').append(` 
+        <tr>
+        <td class = 'tooHigh'>TOO HIGH</td>
+        <td> </td>
+        </tr>
+      `)
+    }
+    if (guess.player1guess < randomNum) {
+      $('#playerTable').append(` 
+        <tr>
+        <td class = 'tooLow'>TOO LOW></td>
+        </tr>
+      `)
+    }
+    if (guess.player1guess == randomNum) {
+      $('#playerTable').append(` 
+      <tr>
+      <td class = 'rightAnswer'>🌟Winner🌟</td>
+      </tr>
+    `)
+    }
+    if (guess.player2guess > randomNum) {
+      $('#playerTable').append(` 
+      <tr>
+      <td> </td>
+      <td class = 'tooHigh'>TOO HIGH</td>
+      </tr>
+    `)
+    }
+    if (guess.player2guess < randomNum) {
+      $('#playerTable').append(` 
+      <tr>
+      <td> </td>
+      <td class = 'tooLow'>TOO LOW</td>
+      </tr>
+    `)
+    }
+    if (guess.player2guess == randomNum) {
+      $('#playerTable').append(` 
+    <tr>
+    <td> </td>
+    <td class = 'rightAnswer'>🌟Winner🌟</td>
+    </tr>
+  `)
+    }
+
+  }
+}
+
+
+
+
+
 //function for restart
-function restartGame (){
+function restartGame() {
   console.log('restart is working too!!')
+  $('#playerTable').empty();
 }
--- server/server.js
+++ server/server.js
@@ -18,12 +18,26 @@ let playersGuess = [
 ]
 
 let randomNum = Math.floor(Math.random () * (25 - 1) +1)
+// console.log('hey', randomNum);
+
+// function compareGuess () {
+//   for (let player of playersGuess){
+//   console.log('in compareGuess', player)
+// }
+// }
+
+let sending = {
+  randomNum:randomNum,
+  playersGuess:playersGuess
+};
+
+// console.log('hey', sending);
 
 
 // GET & POST Routes go here
 app.get('/getguess', (req, res) => {
   console.log('inside GET of guess')
-  res.send(200)
+  res.send(playersGuess);
 })
 
 app.post('/addguess',(req,res) => {
@@ -35,3 +49,5 @@ app.post('/addguess',(req,res) => {
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
 })
+
+
